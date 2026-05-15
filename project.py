322d93d26f3fd0dd223465a2a90a3c664378e2cd
Dalis <sodalisserey@gmail.com>
2026-05-15 17:53:40 +0100

    Add validation check for summer menu

--- project.py
+++ project.py
@@ -3,24 +3,26 @@ from datetime import datetime
 import csv
 
 def main():
-    day_menu_csv = "day_menu.csv"
-    brunch_menu_csv = "brunch_menu.csv"
-    day_menu = get_menu(day_menu_csv)
-    brunch_menu = get_menu(brunch_menu_csv)
+    day_menu = get_menu("day_menu.csv")
+    brunch_menu = get_menu("brunch_menu.csv")
+    summer_menu = get_menu("summer_menu.csv")
+
     order_confirmed = False
     current_date_time = datetime.today().strftime("%A %d/%m/%y %H:%M")
+    current_month = datetime.now().month
     order = {}
 
     name = input(f"\nWelcome to the Café! What is your name? ").strip().title()
-    print(f"\nHello {name}! Check out our menu:\n{format_menu(day_menu, brunch_menu)}"
-          f"\nPlease note brunch is served 10:00-13:00"
+    print(f"\nHello {name}! Check out our menu:\n{format_menu(day_menu, brunch_menu, summer_menu)}"
+          f"\nPlease note brunch is served between 10:00-13:00"
+          f"\nSummer items are served from June to September"
           f"\nEnter 'done' once you've completed your order!"
           f"\n")
 
     while not order_confirmed:
         current_time = datetime.today().strftime("%H:%M")
         request = input(f"What can I get you today, {name}? ").strip().title()
-        order_valid = validate_order(day_menu, brunch_menu, request, current_time)
+        order_valid = validate_order(day_menu, brunch_menu, summer_menu, request, current_time, current_month)
         order_confirmed = confirm_order(request, order)
 
         if order_valid:
@@ -29,7 +31,7 @@ def main():
             order[request] += 1
 
     print(f"\nHere's your receipt, {name}:"
-          f"\n{get_receipt(order, day_menu, brunch_menu, current_date_time)}"
+          f"\n{get_receipt(order, day_menu, brunch_menu, summer_menu, current_date_time)}"
           f"\nYour order should be with you shortly"
           f"\nWe hope to see you again soon ₊˚⊹♡")
 
@@ -47,23 +49,30 @@ def get_menu(menu_csv):
     return menu
 
 
-def format_menu(day_menu, brunch_menu):
+def format_menu(day_menu, brunch_menu, summer_menu):
     formatted_menu = [["A L L - D A Y   M E N U", ""]] + \
                      [[f"{item}", f"£ {price:.2f}"] for item, price in day_menu.items()] + \
                      [["-------------------------", "-------"]] + \
                      [["B R U N C H   M E N U", ""]] + \
-                     [[f"{item}", f"£ {price:.2f}"] for item, price in brunch_menu.items()]
+                     [[f"{item}", f"£ {price:.2f}"] for item, price in brunch_menu.items()] + \
+                     [["-------------------------", "-------"]] + \
+                     [["S U M M E R   M E N U", ""]] + \
+                     [[f"{item}", f"£ {price:.2f}"] for item, price in summer_menu.items()]
 
     return tabulate(formatted_menu, headers=[], tablefmt="fancy_outline", colalign=("left", "right"))
 
 
-def validate_order(day_menu, brunch_menu, request, current_time):
+def validate_order(day_menu, brunch_menu, summer_menu, request, current_time, current_month):
     order_valid = False
     if brunch_menu.get(request):
         if "10:00" > current_time or current_time > "13:00":
-            print("Sorry, brunch is only served between 10:00-13:00")
+            print(f"Brunch? At {current_time}? Why don't we try again?")
         else:
             order_valid = True
+    elif summer_menu.get(request):
+        if current_month < 6 or current_month > 9:
+            print("Nice try, but the summer menu is for summer...")
+        else: order_valid = True
     elif day_menu.get(request):
         order_valid = True
     else:
@@ -90,10 +99,11 @@ def confirm_order(request, order, answer=None):
     return order_confirmed
 
 
-def get_receipt(order, day_menu, brunch_menu, current_datetime):
+def get_receipt(order, day_menu, brunch_menu, summer_menu, current_datetime):
     total = 0
     menus = [[item, price] for item, price in day_menu.items()] + \
-            [[item, price] for item, price in brunch_menu.items()]
+            [[item, price] for item, price in brunch_menu.items()] + \
+            [[item, price] for item, price in summer_menu.items()]
 
     receipt = [[f"{current_datetime}", ""],
                ["-------------------------", "-------"],
